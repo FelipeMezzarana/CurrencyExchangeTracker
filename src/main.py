c3ba43becd635330a347af1e4d755758434de814
Felipe S. Mezzarana <spinelli.mezzarana@gmail.com>
2023-11-16 17:41:18 +0100

    Separate setting for scalability

--- src/main.py
+++ src/main.py
@@ -1,38 +1,26 @@
-# Standard library
-import logging
-
 # First party
 from src.modules import create_report, update_currency_exchange
-
-# Define global logging config
-logging.basicConfig(
-    format="%(levelname)s - Line %(lineno)d (%(name)s %(asctime)s) - %(message)s",
-    datefmt="%Y-%m-%d %H:%M",
-    level=logging.INFO,
-)
-
-
-DB_PATH = "src/database/currency_exchange_db.db"
-BASED_CURRENCY_MAPPING = {"usd": "dollar", "eur": "euro"}
-REPORT_CURRENCY_LIST = ["dkk", "brl", "jpy", "gbp", "cny"]
+from src import settings
 
 
 def run():
     """Execute all steps.
-    * Update currency_exchange_db.db with currency in based_currency_mapping
+    * Update currency_exchange_db.db with currency in based_currency_mapping (settings.py)
     New tables with diffetent based currency may be created by adding new item in BASED_CURRENCY_MAPPING
     items should be added in the format: based_currency:table_prefix
     A list of available currency can be checked here:
     https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json
 
     * Generate an excel report comparing the chosen currency with dollar and euro
-    New currency can be added in REPORT_CURRENCY_LIST, each currency will add a tab in the report
+    New currency can be added in REPORT_CURRENCY_LIST (settings.py)
+    Each currency will add a tab in the report
     """
     # updates the db regardless of the last update date
-    update_currency_exchange.etl_pipeline(BASED_CURRENCY_MAPPING, DB_PATH)
+    update_currency_exchange.etl_pipeline(settings.BASED_CURRENCY_MAPPING, settings.DB_PATH)
     # Generates Excel report
-    create_report.report_pipeline(REPORT_CURRENCY_LIST, DB_PATH)
+    create_report.report_pipeline(settings.REPORT_CURRENCY_LIST, settings.DB_PATH)
 
 
 if __name__ == "__main__":
     run()
+    
---
+++ src/settings.py
@@ -0,0 +1,21 @@
+# Standard library
+import logging
+
+# Define global logging config
+logging.basicConfig(
+    format="%(levelname)s - Line %(lineno)d (%(name)s %(asctime)s) - %(message)s",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.INFO,
+)
+
+# SqlLite db path 
+DB_PATH = "src/database/currency_exchange_db.db"
+# Add new tables with different based currency here 
+# Expected format -> {based_currency:table_prefix}
+BASED_CURRENCY_MAPPING = {
+    "usd": "dollar",
+    "eur": "euro"
+    }
+# Add new currency to the Excel report here 
+# Each currency generate a tab in the report
+REPORT_CURRENCY_LIST = ["dkk", "brl", "jpy", "gbp", "cny"]
